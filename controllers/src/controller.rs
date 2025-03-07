254b36f4cad13c532567008faf47d865c4487131
Pieter Staal <pieterstaal@outlook.com>
2025-03-07 22:37:45 +0100

    Research I2C blocking issues for Si configuration

--- controllers/src/controller.rs
+++ controllers/src/controller.rs
@@ -3,6 +3,7 @@
 /// The controller should have access to the resources that control frequency.
 pub trait FrequencyController<const B: usize> {
     /// Run the frequency control algorithm. This is called at a set interval (every N cycles)
+    /// Therefore, run must always take fewer than N cycles.
     fn run(&mut self, buffer_levels: &[usize]);
     /// Change the amount of neighboring nodes that the controller should assume.
     fn set_degree(&mut self, new_degree: usize);
--- controllers/src/si5351.rs
+++ controllers/src/si5351.rs
@@ -1,5 +1,7 @@
 use embedded_hal::blocking::i2c;
 use fixed::types::I16F16;
+use rp_pico::hal::{i2c as _, I2C};
+use rp_pico::pac::{self, I2C1};
 use si5351::{Si5351, Si5351Device, PLL};
 
 use crate::{
@@ -26,8 +28,30 @@ where
             divider: PLL_FRAC_MAX / 2,
         }
     }
+}
 
+impl<SDA, SCL> Si5351Controller<I2C<I2C1, (SDA, SCL)>> {
+    /// TODO: This blocks and is too slow
+    /// """non""" blocking operation idea:
+    /// - every iteration of this control loop, put one (or FIFO size, or however many fit) byte into the tx fifo of the I2C block.
+    /// - or just use DMA?
+    ///
+    /// or
+    ///
+    /// the config needs an address + 9 bytes, which is less than the 16 bytes that the i2c TX fifo seems to have, so this call _should_
+    /// be non-blocking already, however, run() is certainly called more often than it takes to transmit those 83 bits over a ≤400kHz line.
+    /// So we can monitor the fill level of the tx fifo here to check if we're ready to send.
     fn set_pll_frac(&mut self, frac: u32) {
+        // unsafe and a hack, can we do the ownership in a better way? only works if the SI is connected on I2C1 now...
+        unsafe {
+            let pac = pac::Peripherals::steal();
+            let fill_level = pac.I2C1.ic_txflr().read().txflr().bits();
+
+            // Cannot run I2C non-blocking
+            if fill_level > 0 {
+                return;
+            }
+        }
         let frac = frac & 0x7ffff; // TODO: verify. error?
         self.si
             .setup_pll(PLL::A, 35, frac, 0xfffff)
@@ -38,10 +62,7 @@ where
 const PLL_FRAC_MAX: I16F16 = I16F16::from_bits(0xfffff);
 const PLL_FRAC_MIN: I16F16 = I16F16::from_bits(0x00000);
 
-impl<E, I2C, const B: usize> FrequencyController<B> for Si5351Controller<I2C>
-where
-    I2C: i2c::WriteRead<Error = E> + i2c::Write<Error = E>,
-{
+impl<SDA, SCL, const B: usize> FrequencyController<B> for Si5351Controller<I2C<I2C1, (SDA, SCL)>> {
     fn run(&mut self, buffer_levels: &[usize]) {
         //TODO: remove some common code between controllers
         assert!(buffer_levels.len() >= self.degree); // TODO: return Err?
